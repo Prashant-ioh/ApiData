81b49485110e7dea8cfbb0c2579a4acd5baf81cf
Prashant-ioh <vikky4424@gmail.com>
2024-09-17 17:52:26 +0530

    Add project files.

---
+++ ApiData/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using ApiData.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace ApiData.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost]
+        public IActionResult InsertUser(User user)
+        {
+            string query = @"INSERT INTO users (Name, Email) VALUES (@Name, @Email)";
+            string sqlDataSource = _configuration.GetConnectionString("dbConn");
+
+            using (MySqlConnection myCon = new MySqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Name", user.Name);
+                    myCommand.Parameters.AddWithValue("@Email", user.Email);
+
+                    myCommand.ExecuteNonQuery();
+                    myCon.Close();
+                }
+            }
+
+            return Ok(new { message = "User inserted successfully!" });
+        }
+   
+    }
+}
