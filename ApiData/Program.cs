d503281f1c797ad75ba2074d69cedda67bca867b
Prashant-ioh <vikky4424@gmail.com>
2024-09-18 11:01:41 +0530

    add entity framework

--- ApiData/Program.cs
+++ ApiData/Program.cs
@@ -1,21 +1,54 @@
 using ApiData.Data;
 using Microsoft.EntityFrameworkCore;
 
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddControllers();
+
+//// Make sure you're using the correct name ("dbconn") from appsettings.json
+//var connectionString = builder.Configuration.GetConnectionString("dbconn");
+
+//// Register the DbContext with SQL Server and the correct connection string
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+
+//var app = builder.Build();
+
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+// Bind the configuration for MySQL
+//builder.Services.AddSingleton(builder.Configuration);
+
+ builder.Services.AddControllers();
 // Make sure you're using the correct name ("dbconn") from appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("dbconn");
 
 // Register the DbContext with SQL Server and the correct connection string
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
-
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,8 +57,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
